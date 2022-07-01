28fe71d8f3fed480655826eaa9d74df8a5e87e97
naxinga <ngarconf@hotmail.com>
2022-07-01 18:35:12 +0000

    finalizado

--- src/js/component/formulario.jsx
+++ src/js/component/formulario.jsx
@@ -17,7 +17,7 @@ const Formulario = () => {
                 .then(resp => {
                     console.log(resp.ok); // will be true if the response is successfull
                     console.log(resp.status); // the status code = 200 or code = 400 etc.
-                    console.log(resp.text()); // will try return the exact result as string
+                    //console.log(resp.text()); // will try return the exact result as string
                     return resp.json(); // (returns promise) will try to parse the result as json as return a promise that you can .then for results
                 })
                 .then(data => {
@@ -31,7 +31,37 @@ const Formulario = () => {
         []
     )
 
-    const manejador = (event) => { if (event.key === "Enter"){ setTarea([...valTarea,valInput])}}
+    useEffect (
+        ()=>{
+            //if (valTarea.isArray() || valTarea.length==0 ){}else{
+            fetch('https://assets.breatheco.de/apis/fake/todos/user/naxinga', {
+            method: "PUT",
+            body: JSON.stringify(valTarea),
+            headers: {
+                "Content-Type": "application/json"
+            }
+            })
+            .then(resp => {
+                console.log(resp.ok); // will be true if the response is successfull
+                console.log(resp.status); // the status code = 200 or code = 400 etc.
+                console.log(resp.text()); // will try return the exact result as string
+                return resp.json(); // (returns promise) will try to parse the result as json as return a promise that you can .then for results
+            })
+            .then(data => {
+                //here is were your code should start after the fetch finishes
+               console.log(data); //this will print on the console the exact object received from the server
+            })
+            .catch(error => {
+                //error handling
+                console.log(error);
+            })
+        //};
+        }, 
+    )
+
+    const obj = {label:valInput,done:false};    
+
+    const manejador = (event) => { if (event.key === "Enter"){ setTarea([...valTarea,obj])}}
 
     const borrar = (bor) => {
         let a = [...valTarea]
@@ -40,7 +70,7 @@ const Formulario = () => {
     }   
       
     const imprimir = valTarea.map((valTarea,indice) =>
-        <div id={indice} key={indice} className="text-start linea">{valTarea} <button id="borrador" onClick={()=>borrar(indice)}> X </button></div>
+        <div id={indice} key={indice} className="text-start linea">{valTarea.label} <button id="borrador" onClick={()=>borrar(indice)}> X </button></div>
    
     )
   
